1d84f43e872c07692c35ad8986e75eec4566a072
Asif Imtial <aimtial@gmail.com>
2022-04-28 09:46:51 +0600

    future import at top

--- full_source/onmt/Trainer.py
+++ full_source/onmt/Trainer.py
@@ -16,6 +16,8 @@
 # along with HAN-NMT. If not, see <http://www.gnu.org/licenses/>.
 
 from __future__ import division
+from __future__ import absolute_import
+from __future__ import print_function
 from six.moves import range
 """
 This is the loadable seq2seq trainer library that is
@@ -27,8 +29,6 @@ mechanism things here(i.e. what to do), and leave the strategy
 things to users(i.e. how to do it). Also see train.py(one of the
 users of this library) for the strategy things we do.
 """
-from __future__ import absolute_import
-from __future__ import print_function
 import time
 import sys
 import math
